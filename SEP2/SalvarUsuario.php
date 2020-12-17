772a2132ccf729ffba53f6f3d7be2b3d66a4a8b3
Gilson Saturnino <gilsonsatu@gmail.com>
2020-12-17 10:28:33 -0400

    SEP2 Correção SalvarUsuario.php

--- SEP2/SalvarUsuario.php
+++ SEP2/SalvarUsuario.php
@@ -3,12 +3,12 @@ require_once('Conexao.php');
 
 //pegar valores do formulário (indice do vetor eh igual ao atributo name no form)
 $id = $_POST["id"];
-$email = $_POST["email"];
+$senha = $_POST["senha"];
 
 
 $sql = "update usuarios set senha=? where id=?";
 $sqlprep = $conexao->prepare($sql);
-$sqlprep->bind_param("sssi", $nome, $email, $nome_arquivo, $id);
+$sqlprep->bind_param("si", $senha, $id);
 $sqlprep->execute();
 $msgOk = "Senha atualizada com sucesso.";
 
